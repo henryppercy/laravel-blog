503e9391913e7dc603531442cbf2fc023f8c8256
Henry Percy <henryppercy@gmail.com>
2023-02-20 14:49:26 +0000

    Written HTML/PHP to render list of blog posts and detailed blog post

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1>
+            <?= $post->title; ?>
+        </h1>
+        <div>
+            <?= $post->body; ?>
+        </div>
+        <a href="/">Go Back</a>
+    </article>
+</body>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,17 @@
 <!doctype html>
 
 <title>My blog</title>
+<link rel="stylesheet" href="/app.css">
 
 <body>
-    <h1>Hello World</h1>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <h1>
+                <a href="post/<?= $post->slug; ?>">
+                    <?= $post->title; ?>
+                </a>
+            </h1>
+            <div><?= $post->excerpt; ?></div>
+        </article>
+    <?php endforeach;?>
 </body>
