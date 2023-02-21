7c92723ca8b10a0864e75ae36ab23f85eee7935e
Henry Percy <henryppercy@gmail.com>
2023-02-21 10:48:26 +0000

    Updated seeders and created new migrations for posts and categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,53 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+        $user = \App\Models\User::factory()->create();
+
+         $personal = Category::create([
+             'name' => 'Personal',
+             'slug' => 'personal'
+         ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-family-post',
+            'excerpt' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna porttitor rhoncus dolor purus non.</p>',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna porttitor rhoncus dolor purus non. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Neque ornare aenean euismod elementum nisi quis eleifend. Magna eget est lorem ipsum dolor sit amet consectetur. Sed turpis tincidunt id aliquet risus feugiat in ante metus. Lacus sed viverra tellus in hac habitasse. Nullam ac tortor vitae purus faucibus. Vel fringilla est ullamcorper eget nulla. Consequat ac felis donec et. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna porttitor rhoncus dolor purus non.</p>'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'My Personal Post',
+            'slug' => 'my-personal-post',
+            'excerpt' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna porttitor rhoncus dolor purus non.</p>',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna porttitor rhoncus dolor purus non. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Neque ornare aenean euismod elementum nisi quis eleifend. Magna eget est lorem ipsum dolor sit amet consectetur. Sed turpis tincidunt id aliquet risus feugiat in ante metus. Lacus sed viverra tellus in hac habitasse. Nullam ac tortor vitae purus faucibus. Vel fringilla est ullamcorper eget nulla. Consequat ac felis donec et. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna porttitor rhoncus dolor purus non.</p>'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna porttitor rhoncus dolor purus non.</p>',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna porttitor rhoncus dolor purus non. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Neque ornare aenean euismod elementum nisi quis eleifend. Magna eget est lorem ipsum dolor sit amet consectetur. Sed turpis tincidunt id aliquet risus feugiat in ante metus. Lacus sed viverra tellus in hac habitasse. Nullam ac tortor vitae purus faucibus. Vel fringilla est ullamcorper eget nulla. Consequat ac felis donec et. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna porttitor rhoncus dolor purus non.</p>'
+        ]);
     }
 }
