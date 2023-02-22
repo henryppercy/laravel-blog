89baa26844423c29b3e672d5b69931a4949759f8
Henry Percy <henryppercy@gmail.com>
2023-02-22 14:21:15 +0000

    Added validation messages for the user

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -13,8 +13,13 @@
                            type="text"
                            name="name"
                            id="name"
+                           value="{{ old('name') }}"
                            required
                     >
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -25,8 +30,13 @@
                            type="text"
                            name="username"
                            id="username"
+                           value="{{ old('username') }}"
                            required
                     >
+
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -37,8 +47,13 @@
                            type="email"
                            name="email"
                            id="email"
+                           value="{{ old('email') }}"
                            required
                     >
+
+                    @error('email')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -51,6 +66,10 @@
                            id="password"
                            required
                     >
+
+                    @error('password')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
