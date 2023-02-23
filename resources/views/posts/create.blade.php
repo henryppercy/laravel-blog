68bd6fb8c98c638d374500dfde967d876e7ee94c
Henry Percy <henryppercy@gmail.com>
2023-02-23 21:49:23 +0000

    Refactored to use form components

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,34 +1,27 @@
 <x-layout>
-    <section class="py-8 max-w-md mx-auto">
-        <h1 class="text-lg font-bold mb-4 uppercase">Publish New Post</h1>
-        <x-panel class="max-w-sm mx-auto">
-            <form method="POST" action="/admin/posts" enctype="multipart/form-data">
-                @csrf
 
-                <x-form.input name="title" />
-                <x-form.input name="slug" />
-                <x-form.input name="thumbnail" type="file" />
-                <x-form.textarea name="excerpt" />
-                <x-form.textarea name="body" />
-
-                <div class="mb-6">
-                    <x-form.label name="category"/>
-
-                    <select name="category_id" id="category_id">
-                        @foreach(\App\Models\Category::all() as $category)
-                            <option
-                                value="{{ $category->id }}"
-                                {{ old('category_id') == $category->id ? 'selected' : ''}}>
-                                {{ ucwords($category->name) }}
-                            </option>
-                        @endforeach
-                    </select>
-
-                    <x-form.error name="category"/>
-                </div>
-
-                <x-submit-button>Publish</x-submit-button>
-            </form>
-        </x-panel>
-    </section>
+    <x-setting heading="Publish New Post">
+        <form method="POST" action="/admin/posts" enctype="multipart/form-data">
+            @csrf
+            <x-form.input name="title" />
+            <x-form.input name="slug" />
+            <x-form.input name="thumbnail" type="file" />
+            <x-form.textarea name="excerpt" />
+            <x-form.textarea name="body" />
+            <div class="mb-6">
+                <x-form.label name="category"/>
+                <select name="category_id" id="category_id">
+                    @foreach(\App\Models\Category::all() as $category)
+                        <option
+                            value="{{ $category->id }}"
+                            {{ old('category_id') == $category->id ? 'selected' : ''}}>
+                            {{ ucwords($category->name) }}
+                        </option>
+                    @endforeach
+                </select>
+                <x-form.error name="category"/>
+            </div>
+            <x-submit-button>Publish</x-submit-button>
+        </form>
+    </x-setting>
 </x-layout>
