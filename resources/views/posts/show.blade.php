a5de38428c4b7c9e95c9280d74841484a956d0bf
Henry Percy <henryppercy@gmail.com>
2023-02-23 11:25:24 +0000

    Created form to post comment

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -42,6 +42,14 @@
                         <p>{!! $post->body !!}</p>
                     </div>
                 </div>
+
+                <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    @include('posts._add-comment-form')
+
+                    @foreach($post->comments as $comment)
+                        <x-post-comment :comment="$comment"/>
+                    @endforeach
+                </section>
             </article>
         </main>
     </section>
