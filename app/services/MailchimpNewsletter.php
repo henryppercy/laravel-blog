dc76149581ed69c2c52ad02b09f31c0be8a8ddbe
Henry Percy <henryppercy@gmail.com>
2023-02-23 15:13:36 +0000

    Refactored to be mailchimp specific

--- app/services/MailchimpNewsletter.php
+++ app/services/MailchimpNewsletter.php
@@ -1,26 +1,23 @@
 <?php
 
-namespace App\services;
+namespace App\Services;
 
 use MailchimpMarketing\ApiClient;
 
-class Newsletter
+class MailchimpNewsletter implements Newsletter
 {
+    public function __construct(protected ApiClient $client)
+    {
+        //
+    }
+
     public function subscribe(string $email, string $list = null)
     {
         $list ??= config('services.mailchimp.lists.subscribers');
 
-        return $this->client()->lists->addListMember($list, [
+        return $this->client->lists->addListMember($list, [
             'email_address' => $email,
-            'status'=> 'subscribed'
-        ]);
-    }
-
-    protected function client()
-    {
-        return (new ApiClient())->setConfig([
-            'apiKey' => config('services.mailchimp.key'),
-            'server' => 'us17'
+            'status' => 'subscribed'
         ]);
     }
 }
